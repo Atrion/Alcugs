1323d40a3869261e6e1c4d9fdee0c2fac05d6dc4
The.Modificator <The.Modificator@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2005-04-12 16:41:45 +0000

    added a tclean for temporary MSVC files (and the uruping project file which I've forgotten before)
    git-svn-id: https://alcugs.almlys.org/svn/alcugs/trunk@164 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

---
+++ tclean_msvc.bat
@@ -0,0 +1,9 @@
+REM cleans the temporary MSVC files from the tree
+REM $Id$
+
+tclean
+
+del   "*.ilk"             /f /q
+del   "*.opt"             /f /q
+del   "*.ncb"             /f /q
+del   "*.plg"             /f /q
