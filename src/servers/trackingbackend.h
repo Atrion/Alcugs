37ba2ca8e6e76de366da1f8cbcf5fb2064b6fe49
diafero <diafero@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2008-10-10 19:13:06 +0000

    fix sequence prefix size
    git-svn-id: https://alcugs.almlys.org/svn/private_projects/unet3+@1784 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

--- src/servers/trackingbackend.h
+++ src/servers/trackingbackend.h
@@ -60,7 +60,7 @@ namespace alc {
 		U16 port_start, port_end;
 		Byte externalIp[100]; //!< the external IP (the ones palyers should use to connect to this server)
 		Byte agentGuid[8]; //!< set when isLobby = true, saves the fake guid for UruVision
-		U16 seqPrefix;
+		U32 seqPrefix;
 	};
 	
 	class tPlayer {
