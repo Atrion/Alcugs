403fe80a68f7496b20b3650b2a85097914c5e75f
diafero <diafero@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2009-02-02 10:34:14 +0000

    change some default values
    git-svn-id: https://alcugs.almlys.org/svn/alcugs/trunk@2095 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

--- src/servers/trackingbackend.cpp
+++ src/servers/trackingbackend.cpp
@@ -131,7 +131,7 @@ namespace alc {
 		var = cfg->getVar("tracking.tmp.hacks.agestate");
 		loadAgeState = (var.isNull() || var.asByte());
 		var = cfg->getVar("tracking.tmp.hacks.resetting_ages");
-		if (var.isNull()) strcpy(resettingAges, "Cleft,DniCityX2Finale,GreatZero,Kveer,Myst,Neighborhood02,Personal02,RestorationGuild,spyroom"); // see uru.conf.dist for explanation
+		if (var.isNull()) strcpy(resettingAges, "Cleft,DniCityX2Finale,GreatZero,Kveer,Myst,Neighborhood02,Personal02,RestorationGuild,spyroom,Garden"); // see uru.conf.dist for explanation
 		else strncpy(resettingAges, var.c_str(), 1023);
 		
 		var = cfg->getVar("track.html");
