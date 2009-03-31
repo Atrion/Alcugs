75e3f85835f605a11a93f0fa4f7177d48de71930
diafero <diafero@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2009-03-31 13:40:16 +0000

    Lessen flood limit to better reflect what server and client can cope with
    git-svn-id: https://alcugs.almlys.org/svn/alcugs/trunk@2225 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

--- src/alcnet/unet.cpp
+++ src/alcnet/unet.cpp
@@ -106,9 +106,9 @@ void tUnet::init() {
 	nat_up=128 * 1000;
 	nat_down=512 * 1000;
 	
-	flood_check_sec=2;
-	max_flood_pkts=200; // when first launching a client for an emtpy vault, there are about 100 packets within about 2.5 seconds
-	                    // when linking to MountainScene, huge amount of packets are sent... about 100 per second
+	flood_check_sec=5;
+	max_flood_pkts=300; // when first launching a client for an emtpy vault, there are about 100 packets within about 2.5 seconds
+	                    // when linking to MountainScene, huge amount of packets are sent... about 280 in 5 seconds
 
 	snd_expire=30; //should be enough
 
