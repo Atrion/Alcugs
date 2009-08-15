57d85c694ab87ce080298f02e7527c7da9a5e424
diafero <diafero@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2009-08-15 11:29:30 +0000

    put the configuration options in a different file
    git-svn-id: https://alcugs.almlys.org/svn/alcugs/trunk@2355 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

---
+++ scripts/control-config.sh
@@ -0,0 +1,10 @@
+# Configuration file for control-servers.sh
+root=/home/alcugs/ # change this to the path you work in
+
+# Some more config options which are less likely to change
+basedir=$root/var/
+bindir=$root/bin/
+config=$root/etc/uru.conf
+waittime=0.5 # time to wait after a server was started
+start_servers="tracking vault auth lobby"
+stop_servers="lobby tracking vault auth"
