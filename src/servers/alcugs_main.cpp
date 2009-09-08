793d6ed78f4c193738f48c1412a4ae5e060fd959
diafero <diafero@6ad0db0f-5bf4-0310-8fb6-eb40c444d375>
2009-09-08 16:53:44 +0000

    update changelog; ignore -L option (instead of failing on it)
    git-svn-id: https://alcugs.almlys.org/svn/alcugs/trunk@2385 6ad0db0f-5bf4-0310-8fb6-eb40c444d375

--- src/servers/alcugs_main.cpp
+++ src/servers/alcugs_main.cpp
@@ -133,6 +133,8 @@ int u_parse_arguments(int argc, char * argv[]) {
 			tStrBuf val(argv[i]);
 			opt=opt.substring(2,opt.size()-2);
 			cfg->setVar(val.c_str(),opt.c_str(),"cmdline");
+		} else if(!strcmp(argv[i],"-L")) {
+			lstd->log("The \"-L\" option is no longer supported and will be ignored.\n");
 		} else {
 			parameters_usage();
 			return -1;
