18d79120911761cfd044318e07b0bedec2340efd
diafero <diafero@arcor.de>
2011-02-16 16:45:54 +0100

    merge conv_funs into useful

--- alcbase/alcutil/useful.cpp
+++ alcbase/alcutil/useful.cpp
@@ -36,6 +36,7 @@
 #include <cstring>
 #include <sys/time.h>
 #include <fcntl.h>
+#include <cassert>
 
 #include "alcdebug.h"
 
@@ -134,6 +135,169 @@ void setCloseOnExec(int fd)
 	if (fcntl(fd, F_SETFD, flags) == -1)
 		throw txBase(_WHERE("Error writing flags of FD"));
 }
+
+/** creates a 0000000000000000 valid guid
+	\param guid A hex guid (8 characters)
+	\return A str guid, twice as long as the hex guid
+*/
+tString alcGetStrGuid(const Byte * guid) {
+	return alcHex2Ascii(tMBuf(guid, 8));
+}
+
+/** encodes a 16-character GUID into a 8-byte hex
+	\param out 8-byte array for the output
+	\param in 16-byte string
+*/
+void alcGetHexGuid(Byte *out, tString in) {
+	if (in.size() != 16)
+		throw txUnexpectedData(_WHERE("A GUID string must be 16 characters long"));
+	tMBuf tmp = alcAscii2Hex(in);
+	assert(tmp.size() == 8);
+	memcpy(out, tmp.data(), 8);
+}
+
+/**
+  \brief Converts an hex uid to ascii.
+*/
+tString alcGetStrUid(const Byte * guid) {
+	if(guid==NULL) return "null";
+
+	tString str_guid = alcHex2Ascii(tMBuf(guid,16)), str_guid2;
+	str_guid.rewind();
+	
+	str_guid2.write(str_guid.read(8),8); //ID1
+	str_guid2 += "-";
+	str_guid2.write(str_guid.read(4),4); //ID2
+	str_guid2 += "-";
+	str_guid2.write(str_guid.read(4),4); //ID3
+	str_guid2 += "-";
+	str_guid2.write(str_guid.read(4),4); //ID4
+	str_guid2 += "-";
+	str_guid2.write(str_guid.read(12),12); //ID5
+	assert(str_guid2.size() == 36 && str_guid.tell() == 32);
+	return str_guid2;
+}
+
+/**
+  \brief Converts an Ascii uid to hex
+*/
+void alcGetHexUid(Byte *out, const tString &passed_guid) {
+	if (passed_guid.size() != 36) throw txUnexpectedData(_WHERE("An UID string must be 36 characters long"));
+	if (passed_guid.getAt(8) != '-' || passed_guid.getAt(13) != '-' || passed_guid.getAt(18) != '-' || passed_guid.getAt(23) != '-')
+		throw txUnexpectedData(_WHERE("There must be dashes between the UID parts"));
+
+	tString guid = passed_guid.substring(0, 8) + passed_guid.substring(9, 4) + passed_guid.substring(14, 4) + passed_guid.substring(19, 4) + passed_guid.substring(24, 12);
+	guid = guid.upper();
+	assert(guid.size() == 32);
+	tMBuf tmp = alcAscii2Hex(guid);
+	assert(tmp.size() == 16);
+	memcpy(out, tmp.data(), 16);
+}
+
+/**
+  \brief returns a pointer to a formated time string
+*/
+tString alcGetStrTime(U32 timestamp, U32 microseconds) {
+	char tmptime[26];
+	struct tm * tptr;
+	time_t stamp = timestamp;
+
+	tptr=gmtime(&stamp);
+	strftime(tmptime,25,"%Y:%m:%d-%H:%M:%S",tptr);
+	tString str = tmptime;
+	str.printf(".%06d", microseconds);
+	return str;
+}
+
+tString alcGetStrTime(double stamp, const char format) {
+	U32 time,micros;
+	U32 stampInt = static_cast<U32>(stamp);
+	if(stamp!=0) {
+		switch(format) {
+			case 'u':
+				micros = stampInt % 1000000;
+				time = (stampInt/1000000);
+				break;
+			case 'm':
+				micros = (stampInt*1000) % 1000000;
+				time = (stampInt/1000);
+				break;
+			case 's':
+			default:
+				time = (stampInt);
+				micros = ((stampInt-time)*1000000);
+				DBG(5, "%f = %d . %d\n", stamp, time, micros);
+				break;
+		}
+	} else {
+		time=alcGetTime();
+		micros=alcGetMicroseconds();
+	}
+	return alcGetStrTime(time,micros);
+}
+
+/**
+  \brief Converts hex data to an ASCII string
+*/
+tString alcHex2Ascii(tMBuf in) {
+	tString str;
+	for(U32 n=0; n<in.size(); n++) {
+		Byte i = in.getAt(n);
+		Byte b = ((i & 0xF0)>>4);
+		str.putByte(b<0x0A ? b+0x30 : b+(0x41-0x0A));
+		str.putByte((i & 0x0F)<0x0A ? (i & 0x0F)+0x30 : (i & 0x0F)+(0x41-0x0A));
+	}
+	return str;
+}
+
+/**
+  \brief Converts an ASCII string to hex data
+*/
+tMBuf alcAscii2Hex(tString in) {
+	tMBuf buf;
+	for(U32 n=0; n<in.size()/2; n++) {
+		Byte i1 = in.getAt(2*n), i2 = in.getAt(2*n+1);
+		if ((i1 < 0x41 || i1 > 0x41+25) && (i1 < 0x30 || i1 > 0x30+9))
+			throw txUnexpectedData(_WHERE("There is an invalid character in the data: %c", i1));
+		if ((i2 < 0x41 || i2 > 0x41+25) && (i2 < 0x30 || i2 > 0x30+9))
+			throw txUnexpectedData(_WHERE("There is an invalid character in the data: %c", i2));
+		Byte b =  ((i1)<0x3A ? (i1 - 0x30) : (i1 - (0x41-0x0A)));
+		buf.putByte((0x10 * b) + ((i2)<0x3A ? (i2 - 0x30) : (i2 - (0x41-0x0A))));
+	}
+	return buf;
+}
+
+/**
+	\brief Strips out some characters that win32 doesn't like in file names
+*/
+tString alcStrFiltered(tString what) {
+	tString result;
+	for (U32 i = 0; i < what.size(); ++i) {
+		Byte c = what.getAt(i);
+		if(c!='<' && c!='>' && c!=':' && c!='#' && c!='\\' && c!='/' && c!='*' && c!='?' && c!='"' && c!='\'' && c!='|') {
+			result.putByte(c);
+		}
+	}
+	return result;
+}
+
+
+/** \brief parses a "name[number]" kind of string, setting "t" to the name and returning the number */
+U16 alcParseKey(tString *t) {
+	DBG(9, "alcParseKey() for %s\n", t->c_str());
+	int pos;
+	pos=t->find('[');
+	tString offset;
+	if(pos==-1) return 0;
+	if(!t->endsWith("]")) {
+		throw txParseError(_WHERE("Parse error near %s, malformed var name.\n",t->c_str()));
+	}
+	offset=t->substring(pos+1,t->size()-pos-1);
+	*t = t->substring(0, pos);
+	DBG(9, "   result: %s[%s]\n", t->c_str(), offset.c_str());
+	return offset.asU16();
+}
+
 
 
 }
--- alcbase/alcutil/useful.h
+++ alcbase/alcutil/useful.h
@@ -31,38 +31,99 @@
 
 namespace alc {
 	
-class tString;
+	class tString;
 
-tString alcConsoleAsk();
-bool alcGetLoginInfo(tString argv,tString * username,tString * hostname,U16 * port); // username may be NULL, the rest not
+	tString alcConsoleAsk();
+	bool alcGetLoginInfo(tString argv,tString * username,tString * hostname,U16 * port); // username may be NULL, the rest not
 
-U32 alcGetTime();
-U32 alcGetMicroseconds();
-double alcGetCurrentTime(const char format='s');
-bool alcIsAlpha(char c);
+	U32 alcGetTime();
+	U32 alcGetMicroseconds();
+	double alcGetCurrentTime(const char format='s');
+	bool alcIsAlpha(char c);
 
-void setCloseOnExec(int fd);
+	void setCloseOnExec(int fd);
 
-//These macros are the inverse of htonl() etc. They take little-endian
-// numbers and put them in host order or vice-versa. The htonl() macros
-// cannot be used because they are #defined to no-ops on big-endian machines.
-//Some OSes define htole macros.
-#if !defined(htole16)
- #if defined(WORDS_BIGENDIAN)
-  #define htole16(x) \
-	(((x) >> 8 & 0x00ff) | ((x) << 8 & 0xff00))
-  #define htole32(x) \
-	(((x) >> 24 & 0x000000ff) | ((x) >> 8 & 0x0000ff00) | \
-	 ((x) << 8 & 0x00ff0000) | ((x) << 24 & 0xff000000))
- #else
-  #define htole16(x) (x)
-  #define htole32(x) (x)
- #endif
-#endif
-#if !defined(letoh16)
- #define letoh16(x) htole16(x)
- #define letoh32(x) htole32(x)
-#endif
+	/**
+			\returns a pointer to a formated time string
+	*/
+	tString alcGetStrTime(U32 timestamp, U32 microseconds);
+	tString alcGetStrTime(double stamp=0, const char format='s');
+
+
+	/** Gets an ascii string from hex values
+	*/
+	tString alcHex2Ascii(tMBuf in);
+
+	/** Encodes in Hex, an hex string
+	*/
+	tMBuf alcAscii2Hex(tString in);
+
+
+	// These are mostly special cases of the above to, for compatability with Byte arrays
+	/** creates a 0000000000000000 valid guid
+			\param guid A hex guid (8 characters)
+			\return A str guid, twice as long as the hex guid
+	*/
+	tString alcGetStrGuid(const Byte * guid);
+
+	/** encodes a 16-character GUID into a 8-byte hex
+		\param out 8-byte array for the output
+		\param in 16-byte string
+	*/
+	void alcGetHexGuid(Byte *out, tString in);
+
+	/** creates a 00000000-0000-0000-0000-000000000000 valid guid
+			\param guid A 16 bytes user id
+			\return A 36 bytes str formated id
+	*/
+	tString alcGetStrUid(const Byte * guid);
+
+	/** \param passed_guid A 36 bytes str user id
+		\return A 16 bytes hex user id
+	*/
+	void alcGetHexUid(Byte *out, const tString &passed_guid);
+
+
+	/**
+		Strips out some characters that win32 doesn't like..
+		(Overwrittes what)
+	*/
+	tString alcStrFiltered(tString what);
+
+	/** parses a "name[number]" kind of string, setting "t" to the name and returning the number */
+	U16 alcParseKey(tString *t);
+
+
+	/** convert pageIDs to pageNumbers and the other way around - wired, but whatever... */
+	inline U16 alcPageIdToNumber(U32 seqPrefix, U32 pageId)
+	{
+		return pageId - (seqPrefix << 8) - 33;
+	}
+	inline U32 alcPageNumberToId(U32 seqPrefix, U16 number)
+	{
+		return (seqPrefix << 8) + 33 + number;
+	}
+
+	//These macros are the inverse of htonl() etc. They take little-endian
+	// numbers and put them in host order or vice-versa. The htonl() macros
+	// cannot be used because they are #defined to no-ops on big-endian machines.
+	//Some OSes define htole macros.
+	#if !defined(htole16)
+		#if defined(WORDS_BIGENDIAN)
+			#define htole16(x) \
+				(((x) >> 8 & 0x00ff) | ((x) << 8 & 0xff00))
+			#define htole32(x) \
+				(((x) >> 24 & 0x000000ff) | ((x) >> 8 & 0x0000ff00) | \
+				((x) << 8 & 0x00ff0000) | ((x) << 24 & 0xff000000))
+		#else
+			#define htole16(x) (x)
+			#define htole32(x) (x)
+		#endif
+	#endif
+	#if !defined(letoh16)
+		#define letoh16(x) htole16(x)
+		#define letoh32(x) htole32(x)
+	#endif
 
 }
 
