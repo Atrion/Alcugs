9a95f1569ea25f47cc34e0f0062ebbef2db22776
diafero <diafero@arcor.de>
2011-03-01 11:53:35 +0100

    fix possible race and simplify shutdown logic

--- servers/alcmsgtest.cpp
+++ servers/alcmsgtest.cpp
@@ -24,7 +24,7 @@
 *                                                                              *
 *******************************************************************************/
 
-#define _DBG_LEVEL_ 10
+//#define _DBG_LEVEL_ 10
 #include <alcdefs.h>
 #include <unetbase.h>
 #include <unetmain.h>
