c700a722019708e8dbd88c817defb431ff710dd7
diafero <diafero@arcor.de>
2011-03-21 14:47:27 +0100

    change startup script to work better with multiple alcugs instances on the same machine

--- tools/control-servers.sh
+++ tools/control-servers.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 set -e
 
+# You can have multiple alcugs instances on the same machine, but they must be run by different users or this script will fail!
+
 # configuration
 source ${0%/*}/control-config.sh # the file has to be in the same directory as the script
 export LD_LIBRARY_PATH="$bindir"
 
 # Borrowed from LSB init-functions
 # define the output string colors and text
-ESC=`echo -en "\033"`
+ESC=$(echo -en "\033")
 RESULT_OK="${ESC}[\061;32m${ESC}[70G[\040\040\040OK\040\040\040]${ESC}[m"
 RESULT_WARN="${ESC}[\061;33m${ESC}[70G[\040\040WARN\040\040]${ESC}[m"
 RESULT_FAIL="${ESC}[\061;31m${ESC}[70G[\040FAILED\040]${ESC}[m"
 
 getPid(){
-	mkdir $basedir -p
-	if [[ -f $basedir/$1.pid  ]]; then
-		PID=`cat $basedir/$1.pid`
-		# check if the provess is still running with the same name
-		if [[ ( -n "$PID" ) && ( -n `ps -p $PID | grep alcugs_$1` ) ]]; then
+	mkdir "$basedir" -p
+	if [[ -f "$basedir/$1.pid"  ]]; then
+		PID=$(cat "$basedir/$1.pid")
+		# check if the process is still running with the same name
+		if [[ ( -n "$PID" ) && ( -n $(ps -p $PID -u "$USER" | grep alcugs_$1) ) ]]; then
 			echo $PID
 		else
-			rm $basedir/$1.pid
+			rm "$basedir/$1.pid"
 		fi
 	else
 		# check if we can find the PID without the file
-		PID=`pidof $bindir/alcugs_$prog`
+		PID=$(ps -u "$USER" | grep alcugs_$1 | cut -f 1 -d ' ')
 		if [[ $PID ]]; then
 			echo $PID > $basedir/$1.pid
 			echo $PID
@@ -32,7 +34,7 @@ getPid(){
 	fi
 }
 
-if [[ "`whoami`" == "root" ]]; then
+if [[ "$(whoami)" == "root" ]]; then
 	echo "Don't run the alcugs servers as root!"
 	exit
 fi
@@ -42,13 +44,13 @@ case $1 in
 		ulimit -c unlimited # make sure we get coredumps
 		for prog in $start_servers; do
 			echo -n "Starting $prog... "
-			if [[ -z `getPid $prog` ]]; then
-				mkdir $basedir/$prog -p
-				cd $basedir/$prog
-				$bindir/alcugs_$prog -D -c $config
+			if [[ -z $(getPid $prog) ]]; then
+				mkdir "$basedir/$prog" -p
+				cd "$basedir/$prog"
+				"$bindir/alcugs_$prog" -D -c "$config"
 				RETVAL=$?
 				
-				if [[ ( -n `getPid $prog`) && ( $RETVAL ) ]]; then
+				if [[ ( -n $(getPid $prog)) && ( $RETVAL ) ]]; then
 					echo -e $RESULT_OK
 				else
 					echo -e $RESULT_FAIL
@@ -64,13 +66,13 @@ case $1 in
 	stop)
 		for prog in $stop_servers; do
 			unset PID
-			PID=`getPid $prog`
+			PID=$(getPid $prog)
 			echo -n "Stopping $prog..."
 			if [[ -n $PID ]]; then
 				kill -INT $PID
-				sleep 1
+				sleep 0.5
 				i=0
-				while [[ `getPid $prog` ]]; do
+				while [[ $(getPid $prog) ]]; do
 					echo -n "."
 					if [[ $i -lt 2 ]]; then
 						kill -INT $PID
@@ -91,7 +93,7 @@ case $1 in
 	status)
 		for prog in $start_servers; do
 			echo -n "Checking $prog..."
-			if [[ `getPid $prog` ]]; then
+			if [[ $(getPid $prog) ]]; then
 				echo -n " running"
 				echo -e $RESULT_OK
 			else
